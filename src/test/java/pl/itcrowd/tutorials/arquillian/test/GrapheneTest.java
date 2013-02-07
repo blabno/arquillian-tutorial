6fb34ea1b7c2203e9d0d53b8075e57797640fb44
Bernard Labno <bernard.labno@pjwstk.edu.pl>
2013-02-07 16:54:10 +0100

    Reuse session (run with existing selenium server)

--- src/test/java/pl/itcrowd/tutorials/arquillian/test/GrapheneTest.java
+++ src/test/java/pl/itcrowd/tutorials/arquillian/test/GrapheneTest.java
@@ -1,53 +1,47 @@
 package pl.itcrowd.tutorials.arquillian.test;
 
-import org.jboss.arquillian.ajocado.dom.Event;
-import org.jboss.arquillian.ajocado.framework.GrapheneSelenium;
-import org.jboss.arquillian.ajocado.locator.IdLocator;
-import org.jboss.arquillian.ajocado.locator.JQueryLocator;
 import org.jboss.arquillian.drone.api.annotation.Drone;
+import org.jboss.arquillian.graphene.Graphene;
 import org.jboss.arquillian.junit.Arquillian;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import java.util.List;
 
-import static org.jboss.arquillian.ajocado.Graphene.guardHttp;
-import static org.jboss.arquillian.ajocado.Graphene.guardXhr;
-import static org.jboss.arquillian.ajocado.Graphene.id;
-import static org.jboss.arquillian.ajocado.Graphene.jq;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(Arquillian.class)
 public class GrapheneTest {
 
+    final String AUTOCOMPLETE_ID = "j_idt426:j_idt427";
+
+    final String AUTOCOMPLETE_LIST_ID = AUTOCOMPLETE_ID + "List";
+
     @Drone
-    GrapheneSelenium browser;
-
-    URL url;
-
-    public GrapheneTest() throws MalformedURLException
-    {
-        url = new URL("http://showcase.richfaces.org/");
-    }
+    WebDriver driver;
 
     @Test
-    public void autocomplete()
+    public void someTest()
     {
-//        Given
-        browser.open(url);
-
-//        When
-        browser.click(jq(".rf-pm .rf-pm-top-gr-lbl:contains('Input')"));
-        guardHttp(browser).click(jq(".rf-pm .rf-pm-itm-lbl:contains('rich:autocomplete')"));
-
-        final IdLocator input = id("j_idt426:j_idt427Input");
-        browser.focus(input);
-        browser.type(input, "al");
-        guardXhr(browser).fireEvent(input, Event.KEYPRESS);
-
-//        Then
-        final JQueryLocator child = jq("#j_idt426\\:j_idt427List .rf-au-itm");
-        assertEquals(2, child.size());
+        driver.manage().deleteAllCookies();
+        driver.navigate().to("http://showcase.richfaces.org/");
+        final WebElement inputsTab = driver.findElement(By.xpath("//*[@class='rf-pm']//*[@class='rf-pm-top-gr-lbl'][text()='Inputs']"));
+        final WebElement autoComplete = driver.findElement(By.xpath("//*[@class='rf-pm']//*[@class='rf-pm-itm-lbl'][text()='rich:autocomplete']"));
+        inputsTab.click();
+        autoComplete.click();
+        assertEquals("http://showcase.richfaces.org/richfaces/component-sample.jsf?demo=autocomplete&skin=blueSky", driver.getCurrentUrl());
+        final WebElement autocomplete = driver.findElement(By.id(AUTOCOMPLETE_ID));
+        final WebElement autocompleteList = driver.findElement(By.id(AUTOCOMPLETE_LIST_ID));
+        final WebElement input = autocomplete.findElement(By.className("rf-au-inp"));
+        input.sendKeys("al");
+//        Graphene.waitGui(driver).until(Graphene.element(autocompleteList).isVisible());
+        Graphene.waitGui(driver).until().element(autocompleteList).is().visible();
+        final List<WebElement> sugestions = autocompleteList.findElements(By.className("rf-au-itm"));
+        assertEquals(2, sugestions.size());
+        assertEquals("Alabama", sugestions.get(0).getText());
+        assertEquals("Alaska", sugestions.get(1).getText());
     }
 }
